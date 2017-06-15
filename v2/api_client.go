b39eab0d4ddc60aa421c8dec280587eaac55de00
antihax <antihax@gmail.com>
2017-06-15 18:03:16 +0000

    rebuild esi at 58ab647

--- v2/api_client.go
+++ v2/api_client.go
@@ -64,6 +64,7 @@ type APIClient struct {
 	ContactsApi             *ContactsApiService
 	CorporationApi          *CorporationApiService
 	DogmaApi                *DogmaApiService
+	LocationApi             *LocationApiService
 	MailApi                 *MailApiService
 	PlanetaryInteractionApi *PlanetaryInteractionApiService
 	SearchApi               *SearchApiService
@@ -97,6 +98,7 @@ func NewAPIClient(httpClient *http.Client, userAgent string) *APIClient {
 	c.ContactsApi = (*ContactsApiService)(&c.common)
 	c.CorporationApi = (*CorporationApiService)(&c.common)
 	c.DogmaApi = (*DogmaApiService)(&c.common)
+	c.LocationApi = (*LocationApiService)(&c.common)
 	c.MailApi = (*MailApiService)(&c.common)
 	c.PlanetaryInteractionApi = (*PlanetaryInteractionApiService)(&c.common)
 	c.SearchApi = (*SearchApiService)(&c.common)

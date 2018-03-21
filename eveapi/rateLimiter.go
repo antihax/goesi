c755fe3c7fbb86e6099e89c494332f390c40b714
Tyler Sommer <sommertm@gmail.com>
2018-03-21 11:52:59 -0600

    Replace rateLimiter with golang.org/x/time/rate

--- eveapi/rateLimiter.go
+++ eveapi/rateLimiter.go
@@ -3,53 +3,22 @@ package eveapi
 import (
 	"sync/atomic"
 	"time"
+
+	"golang.org/x/time/rate"
 )
 
 type rateLimiter struct {
-	throttle  chan time.Time
-	rate      time.Duration
-	ticker    *time.Ticker
-	burstRate int
+	lim *rate.Limiter
 }
 
 func newRateLimiter(requestsPerSecond int, burstRate int) *rateLimiter {
-	c := &rateLimiter{
-		throttle:  make(chan time.Time, burstRate),
-		rate:      time.Second / (time.Duration)(requestsPerSecond),
-		burstRate: burstRate,
-	}
-	c.startRatelimiter()
-	return c
-}
-
-func (c *rateLimiter) startRatelimiter() {
-	// Create the timed limiter
-	c.ticker = time.NewTicker(c.rate)
-
-	// Fill the buffer with the burst tokens
-	for i := 0; i < c.burstRate; i++ {
-		c.throttle <- time.Now()
-	}
-
-	// Start the rate limiter
-	go c.tick()
-}
-
-func (c *rateLimiter) tick() {
-	for t := range c.ticker.C {
-		select {
-		case c.throttle <- t:
-		default:
-		}
-	}
-}
-
-func (c *rateLimiter) stop() {
-	c.ticker.Stop()
+	lim := rate.NewLimiter(rate.Limit(requestsPerSecond), burstRate)
+	return &rateLimiter{lim}
 }
 
 func (c *rateLimiter) throttleRequest() {
-	<-c.throttle
+	r := c.lim.Reserve()
+	time.Sleep(r.Delay())
 }
 
 type concurrencyLimiter struct {
